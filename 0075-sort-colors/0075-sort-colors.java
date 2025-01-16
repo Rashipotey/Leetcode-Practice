5356c304ba6862a6b5698386166d6ea6890a092c
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-01-16 23:52:56 +0530

    Time: 2 ms (13.09%), Space: 42.4 MB (8.66%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        for(int i=1;i<nums.length;i++){
+            int j=i-1;
+            int key=nums[i];
+            while(j>=0 && nums[j]>=key){
+                nums[j+1]=nums[j];
+                j--;
+            }
+            nums[j+1]=key;
+        }
+    }
+}
