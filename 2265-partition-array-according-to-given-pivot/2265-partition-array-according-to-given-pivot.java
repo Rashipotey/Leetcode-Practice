e815cfd82d5e2f8b0b8412b208bd15b32b4d7ded
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-03-03 22:02:31 +0530

    Time: 5 ms (85.39%), Space: 68.3 MB (15.73%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -1,21 +1,17 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         int ans[]=new int[nums.length];
-        int numLess=0, numEqual=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]<pivot) numLess++;
-            else if(nums[i]==pivot) numEqual++;
-        }
-        int i=0, j=numLess, k=numEqual+numLess;
-        for(int p=0;p<nums.length;p++){
-            int num=nums[p];
-            if(num<pivot){
-                ans[i++]=num;
-            }else if(num>pivot){
-                ans[k++]=num;
-            }else{
-                ans[j++]=pivot;
+        int p=0, q=nums.length-1;
+        for(int i=0, j=nums.length-1;i<nums.length;i++, j--){
+            if(nums[i]<pivot){
+                ans[p++]=nums[i];
             }
+            if(nums[j]>pivot){
+                ans[q--]=nums[j];
+            }
+        }
+        while(p<=q){
+            ans[p++]=pivot;
         }
         return ans;
     }
