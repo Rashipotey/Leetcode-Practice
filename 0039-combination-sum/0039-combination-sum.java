5b1537860fc8a2b913c6e33eb917899a159e7139
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-05-13 11:00:42 +0530

    Time: 2 ms (82.88%), Space: 44.9 MB (31.9%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,19 +1,20 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<List<Integer>>> dp=new ArrayList<>();
-        for(int i=0;i<=target;i++){
-            dp.add(new ArrayList<>());
+        List<List<Integer>> res=new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(0,target,candidates,new ArrayList<>(),res);
+        return res;
+    }
+    public void backtrack(int c, int target, int [] candidates, List<Integer> path, List<List<Integer>> res){
+        if(target==0){
+            res.add(new ArrayList<>(path));
+            return;
         }
-        dp.get(0).add(new ArrayList<>());
-        for(int candidate:candidates){
-            for(int sum=candidate;sum<=target;sum++){
-                for(List<Integer>combination:dp.get(sum-candidate)){
-                    List<Integer> newCombo=new ArrayList<>(combination);
-                    newCombo.add(candidate);
-                    dp.get(sum).add(newCombo);
-                }
-            }
+        for(int i=c;i<candidates.length;i++){
+            if(candidates[i]>target) break;
+            path.add(candidates[i]);
+            backtrack(i,target-candidates[i],candidates,path,res);
+            path.remove(path.size()-1);
         }
-        return dp.get(target);
     }
 }
