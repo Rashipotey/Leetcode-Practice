a7ce532d2b68c82ca9c117866458fa9679338edd
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-05-17 16:53:40 +0530

    Time: 1 ms (99.93%), Space: 46.7 MB (10.47%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,12 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=1;i<nums.length-1;i++){
-            if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1]){
-                return nums[i];
-            }
+        int xor=nums[0];
+        for(int i=1;i<nums.length;i++){
+            xor=xor^nums[i];
         }
-        if(nums.length!=1 && nums[0]!=nums[1]) return nums[0];
-        else return nums[nums.length-1];
+        return xor;
     }
 }
