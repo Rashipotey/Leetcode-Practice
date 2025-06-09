20579c99a7f1942a345d0df3bf342ce9f1a4c1d9
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-06-09 20:57:58 +0530

    Time: 36 ms (86.07%), Space: 53.8 MB (33.78%) - LeetHub

--- 2746-filter-elements-from-array/2746-filter-elements-from-array.js
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -3,12 +3,12 @@
  * @param {Function} fn
  * @return {number[]}
  */
-function filter(arr, fn) {
-    let filteredArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (fn(arr[i], i)) {
-            filteredArr.push(arr[i]);
+var filter = function(arr, fn) {
+    newArray=new Array();
+    for(let i=0;i<arr.length;i++){
+        if(fn(arr[i],i)){
+            newArray.push(arr[i]);
         }
     }
-    return filteredArr;
-}
+    return newArray;
+};
