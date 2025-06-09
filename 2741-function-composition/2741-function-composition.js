793627add3f14c410a7fe612fea54956d5319944
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-06-09 21:07:20 +0530

    Time: 57 ms (35.4%), Space: 54.8 MB (65.2%) - LeetHub

--- 2741-function-composition/2741-function-composition.js
+++ 2741-function-composition/2741-function-composition.js
@@ -5,8 +5,11 @@
 var compose = function(functions) {
     
     return function(x) {
-        return functions.reduceRight((acc, fn) => fn(acc), x);
-    };
+        for(let i=functions.length-1;i>=0;i--){
+            x=functions[i](x);
+        }
+        return x;
+    }
 };
 
 /**
