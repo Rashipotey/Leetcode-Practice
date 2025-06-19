d627d4defe42a5a362c98cf053afedbdddb08f71
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-06-19 09:37:08 +0530

    Time: 10 ms (8.28%), Space: 46.6 MB (26.28%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.java
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.java
@@ -2,15 +2,20 @@ class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         List<List<Integer>> res=new ArrayList<>();
         int s=0, d=graph.length-1;
-        dfs(graph,s,d,res,new ArrayList<>());
-        return res;
-    }
-    private void dfs(int[][] graph, int s, int d, List<List<Integer>> res, List<Integer> p){
+        Queue<List<Integer>> q=new LinkedList<>();
+        List<Integer> p=new ArrayList<>();
+        q.add(p);
         p.add(s);
-        if(s==d) res.add(new ArrayList<>(p));
-        for(int n:graph[s]){
-            dfs(graph,n,d,res,p);
-            p.remove(p.size()-1);
+        while(!q.isEmpty()){
+            List<Integer> currPath=q.poll();
+            int last=currPath.get(currPath.size()-1);
+            if(last==d) res.add(new ArrayList<>(currPath));
+            for(int n:graph[last]){
+                List<Integer> newP=new ArrayList<>(currPath);
+                newP.add(n);
+                q.add(newP);
+            }
         }
+        return res;
     }
 }
