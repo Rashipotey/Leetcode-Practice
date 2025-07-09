2afecbeac2e45e236e61a5b9dacaab088b7c9514
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-07-09 10:05:41 +0530

    Time: 10 ms (45.56%), Space: 56.9 MB (48.03%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,21 +10,33 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        ArrayList<Integer> arr=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null)
-        {
-            arr.add(temp.val);
-            temp=temp.next;
+        if(head==null || head.next==null) return head;
+        ListNode prev=null, slow=head, fast=head;
+        while(fast!=null && fast.next!=null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        Collections.sort(arr);
-        head=new ListNode(-1);
-        temp=head;
-        for(int num:arr)
-        {
-            temp.next=new ListNode(num);
-            temp=temp.next;
+        prev.next=null;
+        ListNode l1=sortList(head);
+        ListNode l2=sortList(slow);
+        return merge(l1,l2);
+    }
+    private ListNode merge(ListNode l1, ListNode l2){
+        ListNode l=new ListNode(0);
+        ListNode p=l;
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                p.next=l1;
+                l1=l1.next;
+            }else{
+                p.next=l2;
+                l2=l2.next;
+            }
+            p=p.next;
         }
-        return head.next;
+        if(l1!=null) p.next=l1;
+        if(l2!=null) p.next=l2;
+        return l.next;
     }
 }
