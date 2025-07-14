fd9b9bfcda94b61f2818de2ba2d8d9e1dae9a497
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-07-14 17:12:36 +0530

    Time: 16 ms (99.66%), Space: 42.4 MB (59.81%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.java
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.java
@@ -1,23 +1,18 @@
 class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
-        Map<String, Integer> dp=new HashMap<>();
-        return helper(strs,m,n,0,dp);
-    }
-    private int helper(String[] strs, int m, int n, int i, Map<String, Integer> dp){
-        if(i==strs.length) return 0;
-        String key=m+","+n+","+i;
-        if(dp.containsKey(key)) return dp.get(key);
-        int z=0, o=0;
-        for(char c:strs[i].toCharArray()){
-            if(c=='0') z++;
-            else o++;
+        int dp[][]=new int[m+1][n+1];
+        for(String s:strs){
+            int z=0, o=0;
+            for(char c:s.toCharArray()){
+                if(c=='0') z++;
+                else o++;
+            }
+            for(int i=m;i>=z;i--){
+                for(int j=n;j>=o;j--){
+                    dp[i][j]=Math.max(dp[i][j],dp[i-z][j-o]+1);
+                }
+            }
         }
-        int exc=helper(strs,m,n,i+1,dp);
-        int inc=0;
-        if(m>=z && n>=o){
-            inc=1+helper(strs,m-z,n-o,i+1,dp);
-        }
-        dp.put(key,Math.max(exc,inc));
-        return dp.get(key);
+        return dp[m][n];
     }
 }
