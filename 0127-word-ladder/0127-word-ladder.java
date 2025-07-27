0ef1ac4dfbb60efd27e22286e95046c233783872
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-07-27 23:23:38 +0530

    Time: 76 ms (64.59%), Space: 46.1 MB (77.51%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,54 +1,42 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Map<String, List<String>> adj=new HashMap<>();
-        for(int i=-1;i<wordList.size();i++){
-            if(i==-1){
-                adj.put(beginWord, new ArrayList<>());
-            }else{
-                adj.put(wordList.get(i), new ArrayList<>());
-            }
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (!wordSet.contains(endWord)) {
+            return 0;
         }
-        for(String word:wordList){
-            if(isOneLetterDiff(beginWord, word)){
-                adj.get(beginWord).add(word);
-                adj.get(word).add(beginWord);
-            }
-        }
-        for(int i=0;i<wordList.size();i++){
-            for(int j=i+1;j<wordList.size();j++){
-                if(isOneLetterDiff(wordList.get(i), wordList.get(j))){
-                    adj.get(wordList.get(i)).add(wordList.get(j));
-                    adj.get(wordList.get(j)).add(wordList.get(i));
-                }
-            }
-        }
-        Set<String> visit=new HashSet<>();
-        Queue<String> q=new LinkedList<>();
-        q.add(beginWord);
-        visit.add(beginWord);
-        int count=1;
-        while(!q.isEmpty()){
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                 String curr=q.poll();
-                if(curr.equals(endWord)) return count;
-                for(String n:adj.get(curr)){
-                    if(!visit.contains(n)){
-                        q.add(n);
-                        visit.add(n);
+
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        int level = 1;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                String word = queue.poll();
+                char[] wordChars = word.toCharArray();
+
+                for (int j = 0; j < wordChars.length; j++) {
+                    char originalChar = wordChars[j];
+
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        wordChars[j] = c;
+                        String newWord = new String(wordChars);
+
+                        if (newWord.equals(endWord)) {
+                            return level + 1;
+                        }
+
+                        if (wordSet.contains(newWord)) {
+                            queue.offer(newWord);
+                            wordSet.remove(newWord); // mark visited
+                        }
                     }
+                    wordChars[j] = originalChar;
                 }
             }
-            count++;
+            level++;
         }
         return 0;
-    }
-    private boolean isOneLetterDiff(String w1, String w2){
-        if(w1.length()!=w2.length()) return false;
-        int diff=0;
-        for(int i=0;i<w1.length();i++){
-            if(w1.charAt(i)!=w2.charAt(i)) diff++;
-        }
-        return diff==1;
     }
 }
