378369ab367e5c0df01ebf2f837edca7e95ac378
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-08-16 00:30:42 +0530

    Time: 1 ms (23.87%), Space: 40.7 MB (77.96%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        for(int i=0;i<=31;i++){
-            if(Math.pow(4,i)==n) return true;
+        for(int i=0;i<=31;i+=2){
+            if(1<<i==n) return true;
         }
         return false;
     }
