ea8288a0ca0cd296eb8bb49cab397b63609da454
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-09-12 00:33:43 +0530

    Time: 41 ms (61.34%), Space: 44 MB (89.08%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,19 +1,16 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int dp[][]=new int[nums.length+1][nums.length];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        int dp[]=new int[nums.length];
+        Arrays.fill(dp,1);
+        int maxL=1;
+        for(int i=1;i<nums.length;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+            }
+            maxL=Math.max(maxL,dp[i]);
         }
-        return recursion(-1,0,nums,dp);
-    }
-    private int recursion(int prev, int curr, int[] nums, int[][]dp){
-        if(curr==nums.length) return 0;
-        if(dp[prev+1][curr]!=-1) return dp[prev+1][curr];
-        int notTake=recursion(prev,curr+1,nums,dp);
-        int take=0;
-        if(prev==-1 || nums[prev]<nums[curr]){
-            take=1+recursion(curr,curr+1,nums,dp);
-        }
-        return dp[prev+1][curr]=Math.max(take, notTake);
+        return maxL;
     }
 }
