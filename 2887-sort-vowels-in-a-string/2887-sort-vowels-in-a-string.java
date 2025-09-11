31f793852664af7d868f9b79763c2a0ba55d5a5b
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-09-11 14:21:40 +0530

    Time: 17 ms (90.89%), Space: 45.8 MB (87.03%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
@@ -1,22 +1,26 @@
 class Solution {
     public String sortVowels(String s) {
-        List<Character> l=new ArrayList<>();
-        StringBuilder sb=new StringBuilder();
+        int[] count=new int[1000];
+        for(char c:s.toCharArray()){
+            if(isVowel(c)){
+                count[c-'A']++;
+            }
+        }
+        String sortedVowel="AEIOUaeiou";
+        StringBuilder res=new StringBuilder();
+        int j=0;
         for(int i=0;i<s.length();i++){
-            if(isVowel(s.charAt(i))){
-                l.add(s.charAt(i));
-                sb.append('0');
+            if(!isVowel(s.charAt(i))){
+                res.append(s.charAt(i));
             }else{
-                sb.append(s.charAt(i));
+                while(count[sortedVowel.charAt(j)-'A']==0){
+                    j++;
+                }
+                res.append(sortedVowel.charAt(j));
+                count[sortedVowel.charAt(j)-'A']--;
             }
         }
-        Collections.sort(l);
-        for(int i=0, j=0;i<s.length() && j<l.size();i++){
-            if(sb.charAt(i)=='0'){
-                sb.setCharAt(i,l.get(j++));
-            }
-        }
-        return sb.toString();
+        return res.toString();
     }
     private boolean isVowel(char ch){
         if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
