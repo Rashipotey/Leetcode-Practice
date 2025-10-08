108e724a28d6155b14cc0696f62f9a8051cc5edf
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-10-08 21:09:17 +0530

    Time: 32 ms (96.43%), Space: 65.2 MB (45.65%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -11,7 +11,7 @@ class Solution {
         int n=potions.length;
         int l=0, r=n-1;
         while(l<=r){
-            int m=l+(r-l)/2;
+            int m=(l+r)>>>1;
             if((long)x*potions[m]>=s) r=m-1;
             else l=m+1;
         }
