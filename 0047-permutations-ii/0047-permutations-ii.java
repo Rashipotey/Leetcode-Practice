22308ae16daa4c5c9487afd12a3038582a1ab250
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-12-01 16:26:54 +0530

    Time: 3 ms (61.08%), Space: 46.6 MB (23.71%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,19 +1,21 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        Set<List<Integer>> set=new HashSet<>();
-        backtrack(nums,new boolean[nums.length],new ArrayList<>(),set);
-        return new ArrayList<>(set);
+        List<List<Integer>> res=new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(nums,new boolean[nums.length],new ArrayList<>(),res);
+        return res;
     }
-    private void backtrack(int[] nums, boolean[] used, List<Integer> curr, Set<List<Integer>> set){
+    private void backtrack(int[] nums, boolean[] used, List<Integer> curr, List<List<Integer>> res){
         if(curr.size()==nums.length){
-            set.add(new ArrayList<>(curr));
+            res.add(new ArrayList<>(curr));
             return;
         }
         for(int i=0;i<nums.length;i++){
             if(used[i]) continue;
+            if(i>0 && nums[i]==nums[i-1] && !used[i-1]) continue;
             curr.add(nums[i]);
             used[i]=true;
-            backtrack(nums,used,curr,set);
+            backtrack(nums,used,curr,res);
             curr.remove(curr.size()-1);
             used[i]=false;
         }
