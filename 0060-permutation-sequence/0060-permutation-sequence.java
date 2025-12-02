2877ff3a825f70814faad2342515c0ff66a23730
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-12-02 16:08:06 +0530

    Time: 1 ms (95.27%), Space: 42.9 MB (26.59%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,29 +1,18 @@
 class Solution {
     public String getPermutation(int n, int k) {
-        int res[]=new int[1];
-        int kArr[]={k};
-        backtrack(n,kArr,new ArrayList<>(),new boolean[n+1],res);
-        return String.valueOf(res[0]);
-    }
-    private void backtrack(int n, int[] k, List<Integer> li, boolean[] used, int[] res){
-        if(li.size()==n){
-            k[0]--;
-            if(k[0]==0){
-                int num=0;
-                for(int l:li){
-                    num=num*10+l;
-                }
-                res[0]=num;
-                return;
-            }
+        List<Integer> num=new ArrayList<>();
+        for(int i=1;i<=n;i++) num.add(i);
+        StringBuilder sb=new StringBuilder();
+        int fact[]=new int[n];
+        fact[0]=1;
+        for(int i=1;i<n;i++) fact[i]=fact[i-1]*i;
+        k--;
+        for(int i=n;i>=1;i--){
+            int idx=k/fact[i-1];
+            sb.append(num.get(idx));
+            num.remove(idx);
+            k%=fact[i-1];
         }
-        for(int i=1;i<=n;i++){
-            if(used[i]) continue;
-            used[i]=true;
-            li.add(i);
-            backtrack(n,k,li,used,res);
-            used[i]=false;
-            li.remove(li.size()-1);
-        }
+        return sb.toString();
     }
 }
