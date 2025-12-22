2b6071257d5b5364e7ee89ab26dd35daca10e753
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-12-22 13:59:29 +0530

    Time: 29 ms (87.75%), Space: 60.4 MB (8.42%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,26 +1,28 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> li=new ArrayList<>();
-        Set<List<Integer>> set=new HashSet<>();
         Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
+        int n=nums.length;
+        for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1]){
                 continue;
             }
-            int fixed=nums[i];
-            Map<Integer, Integer> map=new HashMap<>();
-            for(int j=i+1;j<nums.length;j++){
-                if(map.containsKey(-(fixed+nums[j]))){
-                    List<Integer> temp=new ArrayList<>();
-                    temp.add(fixed);
-                    temp.add(nums[j]);
-                    temp.add(-(nums[j]+fixed));
-                    set.add(temp);
-                }
-                map.put(nums[j],j);
+            if(nums[i]>0) break;
+            int l=i+1, r=n-1;
+            while(l<r){
+                if(nums[l]+nums[r]==-nums[i]){
+                    li.add(new ArrayList<>());
+                    li.get(li.size()-1).add(nums[l]);
+                    li.get(li.size()-1).add(nums[r]);
+                    li.get(li.size()-1).add(nums[i]);
+                    l++;
+                    r--;
+                    while(l<r && nums[l]==nums[l-1]) l++;
+                    while(l<r && nums[r]==nums[r+1]) r--;
+                }else if(nums[l]+nums[r]>-nums[i]) r--;
+                else l++;
             }
         }
-        li.addAll(set);
         return li;
     }
 }
