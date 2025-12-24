25a43e7071f17ff2f6cd56a335d9de9f327bf164
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2025-12-24 11:14:08 +0530

    Time: 16 ms (80.63%), Space: 46.4 MB (39.02%) - LeetHub

---
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        long sum=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int max=Integer.MIN_VALUE, min=Integer.MAX_VALUE;
+            for(int j=i;j<n;j++){
+                max=Math.max(max,nums[j]);
+                min=Math.min(min,nums[j]);
+                sum+=(max-min);
+            }
+        }
+        return sum;
+    }
+}
