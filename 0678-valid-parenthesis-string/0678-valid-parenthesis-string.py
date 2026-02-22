40a4d5a44b4419b2f339412fd140002279f87f55
Rashi Potey <111111887+Rashipotey@users.noreply.github.com>
2026-02-22 23:52:32 +0530

    Time: 0 ms (100%), Space: 19.4 MB (50.36%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        left=[]
+        star=[]
+        for i, char in enumerate(s):
+            if char=='(':
+                left.append(i)
+            elif char=='*':
+                star.append(i)
+            else:
+                if left:
+                    left.pop()
+                elif star:
+                    star.pop()
+                else:
+                    return False
+        while left and star:
+            if left[-1]<star[-1]:
+                left.pop()
+                star.pop()
+            else:
+                return False
+        return not left
